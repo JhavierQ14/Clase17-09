c3888970cb6b9e325bd2f8ca12af953edf3091b4
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-09-24 12:41:40 -0600

    ClasePresencial

---
+++ SoluParcial/SoluParcial/Controllers/RegistrosController.cs
@@ -0,0 +1,38 @@
+﻿using SoluParcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoluParcial.Controllers
+{
+    public class RegistrosController : Controller
+    {
+        // GET: Registros
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ClsValidacion validacion)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+
+            }
+            else
+            {
+                return View("Index");
+            }
+
+        }
+    }
+}
---
+++ SoluParcial/SoluParcial/Models/ClsValidacion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SoluParcial.Models
+{
+    public class ClsValidacion
+    {
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+
+
+
+        public String Nombre { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+
+        public int Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+        public String Sexo { get; set; }
+
+        [Display(Name = "tipoEstudio")]
+        [Required(ErrorMessage = "Error este campo es requerido")]
+        public String tipoEstudio { get; set; }
+
+
+
+    }
+}
